011ef699ad7add04844203ee1735bad47c138174
Phil Bouchet <pjbouchet@gmail.com>
2019-11-04 12:27:41 +0000

    Update startup message

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
   packageStartupMessage("-----------------------------------------------\n",
                         "dsmextra: version 1.0.0:\n",
                         "-----------------------------------------------\n",
-                        "* Please cite as:\nBouchet PJ, Miller DL, Mannocci L (2019). Extrapolation Assessments\nFor Density Surface Models. R package version 1.0.0.\n",
+                        "* Please cite as:\nBouchet PJ, Miller DL, Mannocci L (2019). A Toolkit for Extrapolation Assessments\nin Density Surface Models. R package version 1.0.0.\n",
                         "\n* Quick start guide:\nA vignette is available at:\nhttps://densitymodelling.github.io/model-extrapolation/vignette/Extrapolation-vignette.html\n",
                         "\n* Further information:\ndsmextra is an output of the DenMod project. For more details, please visit:\nhttps://synergy.st-andrews.ac.uk/denmod/")
 }

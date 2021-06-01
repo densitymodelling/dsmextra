5ff9b8d5669594f9de306c702b1341b407a9a400
Phil Bouchet <pjbouchet@gmail.com>
2021-06-01 14:13:27 +0100

    Version 1.1.5 (website update)

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("-----------------------------------------------\n",
-                        "dsmextra: version 1.1.4:\n",
+                        "dsmextra: version 1.1.5:\n",
                         "-----------------------------------------------\n",
-                        "* Please cite as:\nBouchet PJ, Miller DL, Roberts JJ, Mannocci L, Harris CM, Thomas L (2020). dsmextra: Extrapolation assessment tools for density surface models. Methods in Ecology and Evolution. DOI: 10.1111/2041-210X.13469\n",
-                        "\n* Quick start guide:\nA vignette is available at:\nhttps://densitymodelling.github.io/dsmextra/\n",
+                        "* Please cite as:\nBouchet PJ, Miller DL, Roberts JJ, Mannocci L, Harris CM, Thomas L (2020). dsmextra: Extrapolation assessment tools for density surface models. Methods in Ecology and Evolution, 11(11): 1464-1469. DOI: 10.1111/2041-210X.13469\n",
+                        "\n* Quick start guide:\nA vignette is available at:\nhttps://densitymodelling.github.io/dsmextra/articles/dsmextra.html\n",
                         "\n* Further information:\ndsmextra is an output of the DenMod project. For more details, please visit:\nhttps://synergy.st-andrews.ac.uk/denmod/")
 }
